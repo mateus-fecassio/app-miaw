b9b5b275d27786bd71de4c5f1cc5409df79db020
mateus-fecassio <mateus.fecassio@gmail.com>
2022-07-06 17:14:22 -0300

    miaw parallel

--- voting/miaw_parallel.py
+++ voting/miaw_parallel.py
@@ -1,38 +1,36 @@
 from selenium import webdriver
-
 from selenium.webdriver.common.by import By
-
 import time
 import os
 import threading
 
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 
 
-def init_driver(silent=False): #silent abre sem interface visual
-    options = webdriver.ChromeOptions()
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.service import Service
+# def init_driver(silent=False): #silent abre sem interface visual
+#     options = webdriver.ChromeOptions()
     
-    if silent:
-        options.add_argument("headless")
+#     if silent:
+#         options.add_argument("headless")
 
-    try:
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    except:
-        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-
-    return driver
-
-
-# def init_driver():
-#     chrome_options = webdriver.ChromeOptions()
-#     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-#     chrome_options.add_argument("--headless")
-#     chrome_options.add_argument("--disable-dev-shm-usage")
-#     chrome_options.add_argument("--no-sandbox")
-#     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+#     try:
+#         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+#     except:
+#         driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
 #     return driver
+
+
+def init_driver():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+    return driver
 
 
 def votation(driver, link, person_xpath, vote_counter):
