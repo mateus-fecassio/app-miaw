d487385a06ad34c8bcdd47ca4bf9b410fc8ffda3
mateus-fecassio <mateus.fecassio@gmail.com>
2022-07-06 18:41:10 -0300

    miaw parallel

--- voting/miaw_parallel.py
+++ voting/miaw_parallel.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
-import os
+from time import sleep
+from os import environ
+from threading import Thread
 import gc
-import threading
 
 
 
@@ -25,33 +25,29 @@ import threading
 
 def init_driver():
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.binary_location = environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path=environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
     return driver
 
 
 def votation(driver, link, person_xpath, vote_counter):
-    vote_count = 20
-    
     driver.get(link)
-    time.sleep(12) # x seconds
-    for _ in range(vote_count):	
+    sleep(12) # x seconds
+    for _ in range(20):	
         for _ in range(10):
             try:
-                send_button = driver.find_element(by=By.XPATH, value=person_xpath)
-                send_button.click()
+                driver.find_element(by=By.XPATH, value=person_xpath).click()
                 vote_counter[0] += 1
             except:
                 pass
-            time.sleep(0.1) # x seconds
+            sleep(0.1) # x seconds
         
-        time.sleep(11) # x seconds
-        conf_button = driver.find_element(by=By.XPATH, value='/html/body/div[4]/div/div/div[2]/div[3]/div/div/div/div/div/div[2]/div[2]/div[2]/div/div[3]/div/div')
-        conf_button.click()
+        sleep(11) # x seconds
+        driver.find_element(by=By.XPATH, value='/html/body/div[4]/div/div/div[2]/div[3]/div/div/div/div/div/div[2]/div[2]/div[2]/div/div[3]/div/div').click()
         gc.collect()
     
     # print("Done!")
@@ -59,24 +55,26 @@ def votation(driver, link, person_xpath, vote_counter):
 def main(th=0):
     print(f'Thread {th} -- OK')
 
-    time.sleep(10)
+    sleep(10)
     vote_counter = [0]
 
     link = 'https://miaw.mtv.com.br/vote/streamer-br'
     person_xpath = '/html/body/div[4]/div/div/div[2]/div[3]/div/div/div/div/div/div[2]/div[2]/div[1]/div[1]/div/div[2]/div[1]/div[33]/div/div/div[3]/div/div[5]/div/button' # samira
 
     driver = init_driver()
+    gc.collect()
+
     while True:
         try:
             votation(driver, link, person_xpath, vote_counter)
 
-            text = f'Thread {th} -- NOTE: {vote_counter[0]} votes submitted so far.'
-            print(text)
+            print(f'Thread {th} -- NOTE: {vote_counter[0]} votes submitted so far.')
+            gc.collect()
         except:
-            text = f'Thread {th} -- ERROR: Quiting and restarting driver... The last vote_counter was {vote_counter[0]}'
-            print(text)
+            print(f'Thread {th} -- ERROR: Quiting and restarting driver... The last vote_counter was {vote_counter[0]}')
+            gc.collect()
 
-            time.sleep(60) # x seconds
+            sleep(60) # x seconds
             driver.quit()
             driver = init_driver()
             # driver.refresh()
@@ -85,9 +83,10 @@ def main(th=0):
             driver.refresh()
         except:
             pass
-
+        
+        gc.collect()
 
 if __name__ == "__main__":
     for th in range(2):
-        threading.Thread(target=main, args=([th])).start()
+        Thread(target=main, args=([th])).start()
     # main()
